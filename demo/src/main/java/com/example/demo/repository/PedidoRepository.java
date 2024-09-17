a7dd19801c7ec27662add5ebb7e610ae7c74692b
Rai Araujo <raisanhacu@gmail.com>
2024-09-17 17:14:46 -0300

    Retirando o groupby da 6

--- demo/src/main/java/com/example/demo/repository/PedidoRepository.java
+++ demo/src/main/java/com/example/demo/repository/PedidoRepository.java
@@ -12,18 +12,18 @@ import com.example.demo.models.Pedido;
 // encomendados de maneira presencial nos últimos 8 meses do ano 2023, encomendados
 //  por clientes americanos com limite de crédito maior que 600?
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
-    @Query(value = "SELECT COUNT(p.id_pedido) AS quantidade_pedidos, " +
-                   "COUNT(p.id_pedido) * 100.0 / (SELECT COUNT(DISTINCT p2.id_cliente) " +
-                                                "FROM pedido p2 " +
-                                                "WHERE YEAR(p2.data_prazo) = 2023) AS percentual " +
-                   "FROM pedido p " +
-                   "JOIN item_pedido ip ON p.id_pedido = ip.id_pedido " +
-                   "JOIN cliente c ON p.id_cliente = c.id_cliente " +
-                   "WHERE p.modo_encomenda = 'presencial' " +
-                   "AND c.pais = 'Estados Unidos' AND c.limite_credito > 600 " +
-                   "AND YEAR(p.data_prazo) = 2023 AND p.data_prazo >= '2023-05-01' " +
-                   "GROUP BY p.id_pedido " +
-                   "HAVING SUM(ip.preco * ip.quantidade) > 600", nativeQuery = true)
-    List<Object[]> findPedidosByCliente();
-}
 
+    @Query(value = "SELECT COUNT(DISTINCT p.id_pedido) AS quantidade_pedidos, " +
+            "COUNT(DISTINCT p.id_pedido) * 100.0 / (SELECT COUNT(DISTINCT p2.id_pedido) " +
+            "FROM pedido p2 " +
+            "WHERE YEAR(p2.data_prazo) = 2023) AS percentual " +
+            "FROM pedido p " +
+            "JOIN item_pedido ip ON p.id_pedido = ip.id_pedido " +
+            "JOIN cliente c ON p.id_cliente = c.id_cliente " +
+            "WHERE p.modo_encomenda = 'presencial' " +
+            "AND c.pais = 'Estados Unidos' AND c.limite_credito > 600 " +
+            "AND YEAR(p.data_prazo) = 2023 AND p.data_prazo >= '2023-05-01' " +
+            "HAVING SUM(ip.preco * ip.quantidade) > 600", nativeQuery = true)
+    List<Object[]> findPedidosByCliente();
+
+}
