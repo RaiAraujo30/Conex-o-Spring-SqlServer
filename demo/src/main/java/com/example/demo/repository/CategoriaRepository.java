4648b07cbd3abcb228b1baf94d709d285fc9c0f6
Rai Araujo <raisanhacu@gmail.com>
2024-09-17 17:57:53 -0300

    Mudanças na 5

--- demo/src/main/java/com/example/demo/repository/CategoriaRepository.java
+++ demo/src/main/java/com/example/demo/repository/CategoriaRepository.java
@@ -1,55 +1,38 @@
 package com.example.demo.repository;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
 import com.example.demo.models.Categoria;
 
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
 
+
+    //Consulta 5
     @Query(value = "WITH Faturamento AS ( " +
             "SELECT " +
             "c.id_categoria, " +
             "c.nome AS categoria, " +
-            "SUM(ip.quantidade * ip.preco) AS faturamento, " +
+            "SUM(ip.quantidade * ip.preco) AS total_faturamento, " +
             "YEAR(p.data_prazo) AS ano, " +
             "MONTH(p.data_prazo) AS mes " +
             "FROM " +
             "item_pedido ip " +
-            "JOIN " +
-            "pedido p ON ip.id_pedido = p.id_pedido " +
-            "JOIN " +
-            "produto pr ON ip.id_produto = pr.id_produto " +
-            "JOIN " +
-            "categoria c ON pr.id_categoria = c.id_categoria " +
-            "WHERE " +
-            "p.data_prazo >= DATEADD(MONTH, -6, GETDATE()) " +
-            "AND YEAR(p.data_prazo) BETWEEN YEAR(GETDATE()) - 4 AND YEAR(GETDATE()) " +
-            "GROUP BY " +
-            "c.id_categoria, c.nome, YEAR(p.data_prazo), MONTH(p.data_prazo) " +
-            "), " +
-            "RankedFaturamento AS ( " +
-            "SELECT " +
-            "categoria, " +
-            "faturamento, " +
-            "ano, " +
-            "mes, " +
-            "RANK() OVER (PARTITION BY ano, mes ORDER BY faturamento DESC) AS rnk " +
-            "FROM " +
-            "Faturamento " +
+            "JOIN pedido p ON ip.id_pedido = p.id_pedido " +
+            "JOIN produto pr ON ip.id_produto = pr.id_produto " +
+            "JOIN categoria c ON pr.id_categoria = c.id_categoria " +
+            "WHERE p.data_prazo >= DATEADD(MONTH, -6, GETDATE()) " +  // últimos 6 meses
+            "AND YEAR(p.data_prazo) BETWEEN YEAR(GETDATE()) - 4 AND YEAR(GETDATE()) " +  // últimos 5 anos
+            "GROUP BY c.id_categoria, c.nome, YEAR(p.data_prazo), MONTH(p.data_prazo) " +
+            "), TopCategorias AS ( " +
+            "SELECT F1.ano, F1.mes, F1.categoria, F1.total_faturamento, " +
+            "DENSE_RANK() OVER (PARTITION BY F1.ano, F1.mes ORDER BY F1.total_faturamento DESC) AS ranking " +
+            "FROM Faturamento F1 " +
             ") " +
-            "SELECT " +
-            "categoria, " +
-            "SUM(faturamento) AS total_faturamento " +
-            "FROM " +
-            "RankedFaturamento " +
-            "WHERE " +
-            "rnk <= 8 " +
-            "GROUP BY " +
-            "categoria " +
-            "ORDER BY " +
-            "total_faturamento DESC", nativeQuery = true)
+            "SELECT ano, mes, categoria, total_faturamento " +
+            "FROM TopCategorias " +
+            "WHERE ranking <= 8 " +
+            "ORDER BY ano DESC, mes DESC, total_faturamento DESC", 
+            nativeQuery = true)
     List<Object[]> findCategoriaByFaturamento();
 }
--- demo/src/main/java/com/example/demo/repository/ProdutoRepository.java
+++ demo/src/main/java/com/example/demo/repository/ProdutoRepository.java
@@ -13,82 +13,82 @@ import com.example.demo.models.Produto;
 // entre o preço de venda e o preço de venda mínimo é menor que 2000 E que estão armazenados
 // em pelo menos 5 armazéns distintos.
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-    
-    @Query(value = "SELECT PT.nome, PT.descricao, P.data_garantia " +
-                   "FROM produto P, produto_traducao PT " +
-                   "WHERE (P.preco_venda - P.preco_venda_min)< 2000 AND " +
-                   "P.id_produto = PT.id_produto AND " +
-                   "P.id_produto IN ( SELECT E.id_produto " +
-                                    "FROM estoque E " +
-                                    "GROUP BY E.id_produto " +
-                                    "HAVING COUNT(distinct E.id_armazem)>=5)", nativeQuery = true)
-                   List<Object[]> findProductsByarmazens();
 
-    //consulta 8
-    // Para cada produto, liste o preço de tabela do produto, o preço mínimo de venda
-    //  do produto e o valor de mínimo que o produto já foi vendido em 2024 ou em 2023.
+    @Query(value = "SELECT PT.nome, PT.descricao, P.data_garantia " +
+            "FROM produto P, produto_traducao PT " +
+            "WHERE (P.preco_venda - P.preco_venda_min)< 2000 AND " +
+            "P.id_produto = PT.id_produto AND " +
+            "P.id_produto IN ( SELECT E.id_produto " +
+            "FROM estoque E " +
+            "GROUP BY E.id_produto " +
+            "HAVING COUNT(distinct E.id_armazem)>=5)", nativeQuery = true)
+    List<Object[]> findProductsByarmazens();
+
+    // consulta 8
+    // Para cada produto, liste o preço de tabela do produto, o preço mínimo de
+    // venda
+    // do produto e o valor de mínimo que o produto já foi vendido em 2024 ou em
+    // 2023.
     @Query(value = "SELECT pr.preco_venda AS preco_tabela, " +
-                        "MIN(pr.preco_venda_min) AS preco_minimo_venda, " +
-                        "MIN(CASE WHEN YEAR(p.data_prazo) IN (2023, 2024) " +
-                        "THEN pr.preco_venda_min END) AS preco_minimo_2023_2024 " +
-                    "FROM produto pr " +
-                    "LEFT JOIN " +
-                        "item_pedido ip ON pr.id_produto = ip.id_produto " +
-                    "LEFT JOIN " +
-                        "pedido p ON ip.id_pedido = p.id_pedido " +
-                    "GROUP BY pr.preco_venda", nativeQuery = true)
+            "MIN(pr.preco_venda_min) AS preco_minimo_venda, " +
+            "MIN(CASE WHEN YEAR(p.data_prazo) IN (2023, 2024) " +
+            "THEN pr.preco_venda_min END) AS preco_minimo_2023_2024 " +
+            "FROM produto pr " +
+            "LEFT JOIN " +
+            "item_pedido ip ON pr.id_produto = ip.id_produto " +
+            "LEFT JOIN " +
+            "pedido p ON ip.id_pedido = p.id_pedido " +
+            "GROUP BY pr.preco_venda", nativeQuery = true)
     List<Object[]> findProductsByPrecoVenda();
 
-    // consulta1  //consulta com problema
+    // consulta1 //consulta com problema
     // Listar nome, descrição e preço mínimo de produtos que são fornecidos por
     // fornecedores do Japão com mais de 120 em todos os armazéns.
     @Query(value = "SELECT " +
-                        "pt.nome AS nome_produto, " +
-                        "pt.descricao AS descricao_produto, " +
-                        "p.preco_venda_min AS preco_minimo " +
-                    "FROM " +
-                        "produto p " +
-                    "INNER JOIN " +
-                        "fornecedor f ON p.id_fornecedor = f.id_fornecedor " +
-                    "INNER JOIN " +
-                        "estoque e ON p.id_produto = e.id_produto " +
-                    "INNER JOIN " +
-                        "produto_traducao pt ON p.id_produto = pt.id_produto " +
-                    "WHERE " +
-                        "f.pais = 'Japão' " +
-                    "GROUP BY " +
-                        "pt.nome, pt.descricao, p.preco_venda_min " +
-                    "HAVING " +
-                        "MIN(e.quantidade) > 120", 
-nativeQuery = true)
+            "pt.nome AS nome_produto, " +
+            "pt.descricao AS descricao_produto, " +
+            "p.preco_venda_min AS preco_minimo " +
+            "FROM " +
+            "produto p " +
+            "INNER JOIN " +
+            "fornecedor f ON p.id_fornecedor = f.id_fornecedor " +
+            "INNER JOIN " +
+            "estoque e ON p.id_produto = e.id_produto " +
+            "INNER JOIN " +
+            "produto_traducao pt ON p.id_produto = pt.id_produto " +
+            "WHERE " +
+            "f.pais = 'Japão' " +
+            "GROUP BY " +
+            "pt.nome, pt.descricao, p.preco_venda_min " +
+            "HAVING " +
+            "MIN(e.quantidade) > 120", nativeQuery = true)
 
     List<Object[]> findProductsByFornecedor();
 
-    //consulta 4 //testar consulta
-    // Liste a quantidade e a média da quantidade de produtos em cada estoque que possui 
-    // pelo menos 200 produtos, agrupados pelo armazém, código do estoque e categoria do produto.
+    // consulta 4 //testar consulta
+    // Liste a quantidade e a média da quantidade de produtos em cada estoque que
+    // possui
+    // pelo menos 200 produtos, agrupados pelo armazém, código do estoque e
+    // categoria do produto.
     @Query(value = "SELECT " +
-                        "a.nome AS nome_armazem, " +
-                        "e.codigo AS codigo_estoque, " +
-                        "c.nome AS nome_categoria, " +
-                        "SUM(e.quantidade) AS total_produtos, " +  // soma da quantidade total de produtos no estoque
-                        "AVG(e.quantidade) AS media_produtos " +   // média da quantidade de produtos no estoque
-                    "FROM " +
-                        "estoque e " +
-                    "INNER JOIN " +
-                        "armazem a ON e.id_armazem = a.id_armazem " +
-                    "INNER JOIN " +
-                        "produto p ON e.id_produto = p.id_produto " +
-                    "INNER JOIN " +
-                        "categoria c ON p.id_categoria = c.id_categoria " +
-                    "WHERE " +
-                        "e.quantidade >= 200 " +  // estoques com pelo menos 200 produtos
-                    "GROUP BY " +
-                        "a.nome, e.codigo, c.nome " +
-                    "HAVING " +
-                        "SUM(e.quantidade) >= 200", 
-       nativeQuery = true)
-
+            "a.nome AS nome_armazem, " +
+            "e.codigo AS codigo_estoque, " +
+            "c.nome AS nome_categoria, " +
+            "SUM(e.quantidade) AS total_produtos, " + // soma da quantidade total de produtos no estoque
+            "AVG(e.quantidade) AS media_produtos " + // média da quantidade de produtos no estoque
+            "FROM " +
+            "estoque e " +
+            "INNER JOIN " +
+            "armazem a ON e.id_armazem = a.id_armazem " +
+            "INNER JOIN " +
+            "produto p ON e.id_produto = p.id_produto " +
+            "INNER JOIN " +
+            "categoria c ON p.id_categoria = c.id_categoria " +
+            "WHERE " +
+            "e.quantidade >= 200 " + // estoques com pelo menos 200 produtos
+            "GROUP BY " +
+            "a.nome, e.codigo, c.nome", // agrupamento por armazém, código do estoque e categoria
+            nativeQuery = true)
     List<Object[]> findProductsByEstoque();
 
 }
